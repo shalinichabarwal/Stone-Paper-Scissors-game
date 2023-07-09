8f84cf6951882d3b9c590d5323a58e33beaaa132
Shalini <133792392+shalinichabarwal@users.noreply.github.com>
2023-07-09 19:31:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+var random = Math.floor(Math.random() * 3) + 1;
+
+if (random == 2) {
+  var src = "./Stone, paper, scissor/paper.jpg";
+} else if (random == 1) {
+  var src = "./Stone, paper, scissor/rock.jpg";
+} else {
+  var src = "./Stone, paper, scissor/scissor.jpg";
+}
+
+var array = document.querySelectorAll(".btn");
+for (var i = 0; i < array.length; i++) {
+  array[i].addEventListener("click", game);
+}
+function game(){
+  document.querySelector("#image").setAttribute("src", src);
+
+  if (this.innerHTML == random + "<img src=" +'"'+ src + '">') {
+    document.querySelector(".play").innerHTML = "Draw!";
+  } else if (
+    (this.innerHTML == '1<img src="./Stone, paper, scissor/rock.jpg">' && random == 2) ||
+    (random == 1 && this.innerHTML == '3<img src="./Stone, paper, scissor/scissor.jpg">') ||
+    (random == 3 && this.innerHTML == '2<img src="./Stone, paper, scissor/paper.jpg">')
+  ) {
+    document.querySelector(".play").innerHTML = "Computer won!";
+  } else {
+    document.querySelector(".play").innerHTML = "You won!";
+  }
+}
